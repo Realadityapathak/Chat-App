4753430787d032b6908c7de5873138874b7d3df6
Aditya Pathak <real2adityapathak@gmail.com>
2026-07-27 11:13:22 +0530

    Refactor Message model and getChatPartners function
    Refactored getChatPartners function to use Mongoose schema for Message model.

--- backend/src/models/Message.js
+++ backend/src/models/Message.js
@@ -1,37 +1,29 @@
-export const getChatPartners = async (req, res) => {
-  try {
-    const loggedInUserId = req.user._id;
+import mongoose from "mongoose";
 
-    const messages = await Message.find({
-      $or: [
-        { senderId: loggedInUserId },
-        { receiverId: loggedInUserId },
-      ],
-    }).sort({ createdAt: -1 });
-
-    const chatPartnerIds = [
-      ...new Set(
-        messages.map((msg) =>
-          msg.senderId.toString() === loggedInUserId.toString()
-            ? msg.receiverId.toString()
-            : msg.senderId.toString()
-        )
-      ),
-    ];
-
-    const chatPartners = await User.find({
-      _id: { $in: chatPartnerIds },
-    }).select("-password");
-
-    const orderedPartners = chatPartnerIds
-      .map((id) =>
-        chatPartners.find((user) => user._id.toString() === id)
-      )
-      .filter(Boolean);
-
-    res.status(200).json(orderedPartners);
-  } catch (error) {
-    console.error("Error in getChatPartners:", error.message);
-    res.status(500).json({ error: "Internal server error" });
+const messageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    text: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
   }
-};
+);
+
+const Message = mongoose.model("Message", messageSchema);
+
+export default Message;
